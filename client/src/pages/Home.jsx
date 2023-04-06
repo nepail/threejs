5449829df68c74d9b519d2e4abf84a1987e47ac5
Nepail <kls18z@hotmail.com>
2023-04-06 17:21:47 +0800

    import Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
+import {useSnapshot} from 'valtio'
+import {
+  headContainerAnimation,
+  headtContainerAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion'
 
 const Home = () => {
   return (
